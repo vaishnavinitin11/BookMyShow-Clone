c63a76057775e6be36946bbeedf51e73782ab75f
Vaishnavi Uttarkar <vaishnavinitin11@gmail.com>
2021-08-03 23:48:12 +0530

    tailwind css SetUp

---
+++ planning.js
@@ -0,0 +1,17 @@
+// Book my show planning
+
+// -> we need a reusable carousel
+//      -> Hero carousel
+//      -> Common carousel
+//      -> cast and crew small carousel
+
+
+
+// Pages
+// -> Home Page
+// -> Movie Page
+// -> Categories Page
+// -> Authentication Page
+// -> RazorPay payment gateway
+
+// Plan -> How are we building it??? 
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 //import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    
+    <button className="border-2 px-2 py-1 border-blue-500 md:border-red-500 lg:border-green-500 rounded bg-blue-500 md:bg-red-500 lg:bg-green-500 text-white hover:bg-transparent hover: text-border-2 hover:text-blue-500 md:hover:text-red-500 lg:hover:text-green-500 outline-none focus:outline-none focus:ring md:focus:border-red-300 lg:focus:border-green-500">
+      hello tailwind
+    </button>
+  </div>;
 }
 
 export default App;
