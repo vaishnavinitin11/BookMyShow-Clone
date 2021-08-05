acc6fc22ef7a82c0b87416b51b3b68150db0270a
Vaishnavi Uttarkar <vaishnavinitin11@gmail.com>
2021-08-05 23:43:36 +0530

    Navbar Completed

--- src/components/Navbar/navbar.component.jsx
+++ src/components/Navbar/navbar.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BiChevronRight, BiSearch} from "react-icons/bi";
+import { BiChevronRight, BiSearch, BiChevronDown, BiMenu } from "react-icons/bi";
 
 const NavSm = () => {
   return (
@@ -18,8 +18,54 @@ const NavSm = () => {
     </>
   );
 };
-const NavMd = () => {};
-const NavLg = () => {};
+
+const NavMd = () => {
+  return (
+      <div className="w-full flex items-center gap-3 bg-white px-3 py-2 rounded-md">
+        <BiSearch />
+        <input 
+          type="search" 
+          className="w-full bg-transparent border-none focus:outline-none " 
+          placeholder="Search for Movies, Events, Plays, Sports and Activities."
+        />
+      </div>
+  );
+};
+
+const NavLg = () => {
+  return (
+    <>
+      <div className="container mx-auto px-4 flex items-center justify-between">
+        <div className="flex items-center w-1/2 gap-3">
+          <div className="w-12 h-12" >
+            <img 
+              src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png" 
+              alt="logo"
+              className="w-full h-full"
+            />
+          </div>
+          <div className="w-full flex items-center gap-3 bg-white px-3 py-2 rounded-md">
+            <BiSearch />
+            <input 
+              type="search" 
+              className="w-full bg-transparent border-none focus:outline-none " 
+              placeholder="Search for Movies, Events, Plays, Sports and Activities."
+            />
+          </div>
+        </div>
+        <div className="flex items-center gap-3">
+          <span className="text-gray-200 text-xs flex items-center hover:text-white cursor-pointer"> 
+            Mumbai <BiChevronDown />
+          </span> 
+          <button className="bg-red-600 text-white px-2 py-1 text-sm rounded">Sign in</button>
+          <div className="w-8 h-8 text-white">
+            <BiMenu className="w-full h-full" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 const Navbar = () => {
   return (
@@ -29,8 +75,14 @@ const Navbar = () => {
           {/* Mobile screen */}
           <NavSm />
         </div>
-        <div className="hidden md:flex lg:hidden">{/* Medium/Tab screen */}</div>
-        <div className="hidden lg:flex">{/* Large screen */}</div>
+        <div className="hidden md:flex lg:hidden ">
+          {/* Medium/Tab screen */}
+          <NavMd />
+        </div>
+        <div className="hidden lg:flex">
+          {/* Large screen */}
+          <NavLg />
+        </div>
       </nav>
     </>
   );
