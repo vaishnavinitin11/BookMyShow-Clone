280e56dfbe3e6bfe971187aba2b5aaf974987f61
Vaishnavi Uttarkar <vaishnavinitin11@gmail.com>
2021-08-09 12:52:23 +0530

    add Entertainment Carousel

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -6,7 +6,10 @@ import EntertainmentCardSlider from "../components/Entertainment/Entertainmentca
 const HomePage = () => {
     return (
         <>
-            <EntertainmentCardSlider />
+            <div className="container mx-auto px-4">
+                <h1 className="text-2xl font-bold text-gray-800 my-3">The Best of Entertainment</h1>
+                <EntertainmentCardSlider />
+            </div>
         </>
     );
 };
