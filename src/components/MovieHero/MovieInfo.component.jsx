4124ac8bcc60c7530a6aec85de5bf3279a6952eb
Vaishnavi Uttarkar <vaishnavinitin11@gmail.com>
2021-08-16 14:58:43 +0530

    add movie context

--- src/components/MovieHero/MovieInfo.component.jsx
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+
+
+// context
+import { MovieContext } from "../../context/movie.context";
 
 const MovieInfo = () => {
+
+    const { movie } = useContext(MovieContext);
+//optional chaining
+    const genres = 
+        movie.genres?.map(({name}) => name).join(", ");
+
     return (
         <>
             <div className="flex flex-col gap-3 lg:gap-8">
@@ -16,11 +26,13 @@ const MovieInfo = () => {
                         Streaming now
                     </span>
                 </div>
-                <h1 className="hidden lg:block text-white lg:text-5xl font-bold"> Justice League</h1>
+                <h1 className="hidden lg:block text-white lg:text-5xl font-bold"> 
+                    {movie.original_title}
+                </h1>
                 <div className="flex flex-col-reverse gap-3 lg:gap-5 lg:flex-col">
                     <div className="text-white font-light flex flex-col gap-2 md:px-4">
-                        <h4>4k &bull; English &bull; Action &bull;</h4>
-                        <h4>1h 53m &bull; Action, Sci-Fi, Thriller &bull; 13+ &bull;</h4>
+                        <h4>4k &bull; {movie.original_language}</h4>
+                        <h4>{(movie.runtime / 60).toFixed(2)} h &bull; {genres} &bull; 13+ &bull;</h4>
                     </div>
                     <div className="flex items-center gap-3 md:px-4 md:w-screen lg:w-full">
                         <button className="bg-vaish-600 w-full py-3 text-white font-semibold rounded-lg">
